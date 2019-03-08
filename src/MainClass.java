8663dd351d21459c8657dd49ab5c3c5ddbd90715
issuser <issuser@ISS-CR2919A.stf.nus.edu.sg>
2019-03-08 15:16:46 +0800

    integrate with main.class

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,7 +1,10 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainClass {
+	private static ToDoList list = new ToDoList();
+	
 	public static void main(String[] args){
 		boolean exit = false;
 		do {
@@ -13,31 +16,54 @@ public class MainClass {
 	
 	public static void showMenu(){
 		System.out.println("Welcome to To Do List Application");
-		showCurrentList();
+		//showCurrentList();
 		System.out.println("Menu: ");
 		System.out.println("1. List all to do list");
 		System.out.println("2. Add new item");
 		System.out.println("3. Delete an item");
-		System.out.println("4. Mark an item as complete");
+		//System.out.println("4. Mark an item as complete");
 		System.out.println("9. Quit");
 		System.out.print("Enter your choice: ");
 	}
 	
 	public static int readInput(){
 		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();	
+		int input = scanner.nextInt();
+		scanner.nextLine();
 		
 		switch(input){
 		case 1:
-			System.out.println("1");
+			System.out.println("All items:");
+			List<Item> items = list.getItems();
+			for (Item item: items) {
+				System.out.printf("%s: %s (%s)\n", item.getId(), item.getTitle(), item.getDueDate());
+			}
 			break;
 			
-		case 2:
-			System.out.println("2");
+		case 2: {
+			System.out.println("Add New Item:");
+			
+			System.out.println("Id: ");
+			String id = scanner.nextLine();
+			System.out.println("Title: ");
+			String title = scanner.nextLine();
+			System.out.println("Due Date: ");
+			String dueDate = scanner.nextLine();
+			
+			Item item = new Item(id, title, dueDate);
+			list.addItem(item);
+		}
+			
 			break;
 			
-		case 3:
-			System.out.println("3");
+		case 3: {
+			System.out.println("Delete Item:");
+			
+			System.out.println("Id: ");
+			String id = scanner.nextLine();
+			
+			list.deleteItem(id);
+		}
 			break;
 			
 		case 9:
@@ -47,7 +73,7 @@ public class MainClass {
 			System.out.println("Invalid choice");
 		}
 		
-		
+		System.out.println("");
 		return input;
 	}
 	
